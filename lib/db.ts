36c505187b7f7399d5483441a58b10e61a3f30ba
Joel Udu <joel.udu@coop.co.uk>
2026-01-03 17:38:10 +0000

    fix: pass database url to prisma client constructor

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined
+const prismaClientSingleton = () => {
+    return new PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL,
+            },
+        },
+    })
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+    prisma: ReturnType<typeof prismaClientSingleton> | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
